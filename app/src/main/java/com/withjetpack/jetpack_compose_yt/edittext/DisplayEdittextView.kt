ce7de310e05dc597a3d786870047114347446c44
Chayan <chayan@Chayans-MacBook-Pro.local>
2022-11-17 15:47:41 +0530

    Edittext example in Jetpack Compose

--- app/src/main/java/com/withjetpack/jetpack_compose_yt/edittext/DisplayEdittextView.kt
+++ app/src/main/java/com/withjetpack/jetpack_compose_yt/edittext/DisplayEdittextView.kt
@@ -1,9 +1,12 @@
 package com.withjetpack.jetpack_compose_yt.edittext
 
+import android.inputmethodservice.KeyboardView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -12,6 +15,9 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,13 +36,15 @@ fun DisplayEdittextView() {
             value = text,
             onValueChange = { text = it },
             label = { Text("username") },
-            placeholder = { Text(text = "user Name")}
-
-        )
+            placeholder = { Text(text = "user Name")},
+         //   Icon(painter = painterResource(id = R.drawable.capture_more_images), contentDescription ="" ,tint = colorResource(R.color.disabled)),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+         )
 
         OutlinedTextField(value =text1 , onValueChange = { text=it },
             label = { Text(text = "password")},
             placeholder = { Text(text = "password")},
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
             )
 
     }
